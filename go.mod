578b7021388a897388b1283d21c08202ad68e206
Enan01 <enanzhou@foxmail.com>
2022-11-07 20:04:01 +0800

    tmp: support cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Enan01/notion_bill
 
 go 1.15
 
-require github.com/go-resty/resty/v2 v2.7.0 // indirect
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/urfave/cli/v2 v2.23.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	cli "github.com/urfave/cli/v2"
+)
 
 func main() {
 	fmt.Println("hello notion_bill")
+
+	app := &cli.App{
+		Name:   "notion_bill",
+		Usage:  "import your alipay and wechat bill into notion",
+		Action: func(*cli.Context) error { panic("not implemented") },
+	}
 }
